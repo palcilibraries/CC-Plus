02c3e3755e0c8568ee56b8e72c6309581e07ffb3
Sarah Dooley <dooley.sarah.e@gmail.com>
2020-02-27 22:09:28 -0500

    adding basic layout for institutions

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Admin Dashboard</div>
-<h1>HI SCOTT!</h1>
+
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
--- resources/views/institutions/show.blade.php
+++ resources/views/institutions/show.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="row">
     <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
+        <div class="page-header">
             <h1>{{ $institution->name }}</h1>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('institutions.index') }}"> Back</a>
+        <div class="page-action">
+<!--            <a class="btn btn-primary" href="{{ route('institutions.index') }}"> Back</a>-->
+			<a class="btn btn-primary" href="#">Delete</a>
         </div>
     </div>
 </div>
@@ -16,50 +17,48 @@
 <!-- maybe a vendor-selector tied to a jquery update           -->
 <!-- split-screen vertically? inst-left, sushi details right?  -->
 
-<div class="row details">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-		<h2>Details</h2>
-		<a href="#">edit</a> <em>can we make this swap in the edit view?</em>
-        <div class="form-group">
-            <strong>Type:</strong>
-            {{ $institution->institutiontype->name }}
-        </div>
-        <div class="form-group">
-          <strong>Groups:</strong>
-          @foreach($groups as $group_id => $group_name)
-             @if($institution->isAMemberof($group_id))
-                <label class="badge badge-success">{{ $group_name }} </label>
-             @endif
-          @endforeach
-        </div>
-        <div class="form-group">
-            <strong>FTE:</strong>
-            {{ $institution->fte }}
-        </div>
-        <div class="form-group">
-            <strong>Visibility:</strong>
-            <em>what goes here?</em>
-        </div>
-        <div class="form-group">
-            <strong>Status:</strong>
-            {{ $institution->is_active ? 'Active' : 'Inactive' }}
-        </div>
-        <div class="form-group">
-            <strong>Notes:</strong>
-            {{ $institution->notes }}
-        </div>
-
-    </div>
+<div class="details">
+	<h2 class="section-title">Details</h2>
+	<a href="#" class="section-action">edit</a> <em>can we make this swap in the edit view?</em>
+	<div class="form-group">
+	    <strong>Type:</strong>
+	    {{ $institution->institutiontype->name }}
+	</div>
+	<div class="form-group">
+	  <strong>Groups:</strong>
+	  @foreach($groups as $group_id => $group_name)
+	     @if($institution->isAMemberof($group_id))
+	        <label class="badge badge-success">{{ $group_name }} </label>
+	     @endif
+	  @endforeach
+	</div>
+	<div class="form-group">
+	    <strong>FTE:</strong>
+	    {{ $institution->fte }}
+	</div>
+	<div class="form-group">
+	    <strong>Visibility:</strong>
+	    <em>placeholder</em>
+	</div>
+	<div class="form-group">
+	    <strong>Status:</strong>
+	    {{ $institution->is_active ? 'Active' : 'Inactive' }}
+	</div>
+	<div class="form-group">
+	    <strong>Notes:</strong>
+	    {{ $institution->notes }}
+	</div>
 </div>
 
 <div class="users">
-	<h2>Users</h2>
-	<a href="#">add new</a>
+	<h2 class="section-title">Users</h2>
+	<a href="#" class="section-action">add new</a>
+	<em>associated user list here</em>
 </div>
 
 <div class="related-list">
-	<h2>Providers</h2>
-	<a href="#">add new</a>
+	<h2 class="section-title">Providers</h2>
+	<a href="#" class="section-action">add new</a>
 	
 	<div>[connect provider dropdown - shows form when selected]</div>
 	

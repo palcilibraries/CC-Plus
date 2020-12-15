b06fc78bb90fe6ebae2257516ade479c71a7a4cd
Scott Ross <lsr2912@gmail.com>
2020-12-15 15:00:32 -0500

    ProviderDataTable - Dialogs
    Page-replacing content for import and create functions now uses vuetify pop-over dialogs.

--- resources/js/components/ProviderDataTable.vue
+++ resources/js/components/ProviderDataTable.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <div class="status-message" v-if="success || failure">
-      <span v-if="success" class="good" role="alert" v-text="success"></span>
-      <span v-if="failure" class="fail" role="alert" v-text="failure"></span>
-    </div>
-    <div v-if="showForm==''">
+    <div>
       <v-row v-if="is_admin">
         <v-col cols="2"><v-btn small color="primary" @click="importForm">Import Providers</v-btn></v-col>
         <v-col><v-btn small color="primary" @click="createForm">Create a Provider</v-btn></v-col>
@@ -16,6 +12,10 @@
             <a :href="'/providers/export/xlsx'">.xlsx</a>
         </v-col>
       </v-row>
+      <div class="status-message" v-if="success || failure">
+        <span v-if="success" class="good" role="alert" v-text="success"></span>
+        <span v-if="failure" class="fail" role="alert" v-text="failure"></span>
+      </div>
       <v-data-table :headers="headers" :items="mutable_providers" item-key="prov_id" :options="mutable_options"
                      :key="dtKey" @update:options="updateOptions">
         <template v-slot:item="{ item }">
@@ -34,65 +34,95 @@
         </template>
       </v-data-table>
     </div>
-    <div v-if="showForm=='import'" style="width:50%; display:inline-block;">
-      <v-file-input show-size label="CC+ Import File" v-model="csv_upload" accept="text/csv" outlined></v-file-input>
-      <p>
-        <strong>Providers cannot be deleted during an import operation.</strong><br />
-        <strong>Note: </strong>Provider imports always function as full-replacement updates. Any settings for columns
-        C-G which are NULL, blank, or missing in the import file will cause the import to overwrite the field(s) for
-        the provider with the blank, empty, or NULL value.
-      </p>
-      <p>
-        Provider-IDs (column-A) should be sequential, but this is not required. Importing rows with new, unique ID
-        values will create new providers. Provider names (column-B) must be unique. Attempting to renamed a provider
-        to an existing name will be ignored.
-      </p>
-      <p>
-        Providers can be renamed via import by giving the ID in column-A and the replacement name in column-B. Be
-        aware that the new name takes effect immediately, and will be associated with all harvested usage data that
-        may have been collected using the OLD name (data is stored by the ID, not the name.)
-      </p>
-      <p>
-        For these reasons, use caution when using this import function. Generating a Provider export FIRST will supply
-        detailed instructions for importing on the "How to Import" tab. Generating a new Provider export following an
-        import operation is a good way to confirm that all the settings are as-desired.
-      </p>
-      <v-btn small color="primary" type="submit" @click="importSubmit">Run Import</v-btn>
-      <v-btn small type="button" @click="hideForm">cancel</v-btn>
-    </div>
-    <div v-if="showForm=='create'" style="width:50%; display:inline-block;">
-      <h4>Create a new Provider</h4>
-      <form method="POST" action="" @submit.prevent="formSubmit" @keydown="form.errors.clear($event.target.name)" class="in-page-form">
-        <v-text-field v-model="form.name" label="Name" outlined></v-text-field>
-        <v-switch v-model="form.is_active" label="Active?"></v-switch>
-        <div v-if="is_admin">
-            <v-select outlined required :items="institutions" v-model="form.inst_id" value="current_user.inst_id"
-                      label="Institution" item-text="name" item-value="id"
-            ></v-select>
-        </div>
-        <div v-else>
-            <v-text-field outlined readonly label="Institution" :value="inst_name"></v-text-field>
-        </div>
-        <v-text-field v-model="form.server_url_r5" label="SUSHI Service URL" outlined></v-text-field>
-        <div class="field-wrapper has-label">
-            <v-subheader v-text="'Run Harvests Monthly on Day'"></v-subheader>
-            <v-text-field v-model="form.day_of_month" label="Day-of-Month" hide-details single-line type="number"
-            ></v-text-field>
-        </div>
-        <div class="field-wrapper has-label">
-            <v-subheader v-text="'Reports to Harvest'"></v-subheader>
-            <v-select :items="master_reports" v-model="form.master_reports" value="provider.reports" item-text="name"
-                      item-value="id" label="Select" multiple chips hint="Choose which reports to harvest"
-                      persistent-hint
-            ></v-select>
-        </div>
-        <p>&nbsp;</p>
-        <v-btn small color="primary" type="submit" :disabled="form.errors.any()">
-          Save New Provider
-        </v-btn>
-		<v-btn small type="button" @click="hideForm">cancel</v-btn>
-      </form>
-    </div>
+    <v-dialog v-model="importDialog" max-width="1200px">
+      <v-card>
+        <v-card-title>Import Providers</v-card-title>
+        <v-spacer></v-spacer>
+        <v-card-subtitle><strong>Providers cannot be deleted during an import operation.</strong>
+        </v-card-subtitle>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-file-input show-size label="CC+ Import File" v-model="csv_upload" accept="text/csv" outlined
+              ></v-file-input>
+              <p>
+                <strong>NOTE: </strong>Provider imports always function as full-replacement updates. Any settings for
+                columns C-G which are NULL, blank, or missing in the import file will cause the import to overwrite the
+                field(s) for the provider with the blank, empty, or NULL value.
+              </p>
+              <p>
+                Provider-IDs (column-A) should be sequential, but this is not required. Importing rows with new, unique
+                ID values will create new providers. Provider names (column-B) must be unique. Attempting to renamed a
+                provider to an existing name will be ignored.
+              </p>
+              <p>
+                Providers can be renamed via import by giving the ID in column-A and the replacement name in column-B.
+                Be aware that the new name takes effect immediately, and will be associated with all harvested usage
+                data that may have been collected using the OLD name (data is stored by the ID, not the name.)
+              </p>
+              <p>
+                For these reasons, use caution when using this import function. Generating a Provider export FIRST will
+                supply detailed instructions for importing on the "How to Import" tab. Generating a new Provider export
+                following an import operation is a good way to confirm that all the settings are as-desired.
+              </p>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-col class="d-flex">
+            <v-btn x-small color="primary" type="submit" @click="importSubmit">Run Import</v-btn>
+          </v-col>
+          <v-col class="d-flex">
+            <v-btn class='btn' x-small type="button" color="primary" @click="importDialog=false">Cancel</v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="provDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span>Create a new provider</span>
+        </v-card-title>
+        <form method="POST" action="" @submit.prevent="formSubmit" @keydown="form.errors.clear($event.target.name)" class="in-page-form">
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-text-field v-model="form.name" label="Name" outlined></v-text-field>
+              <v-switch v-model="form.is_active" label="Active?"></v-switch>
+              <div v-if="is_admin">
+                <v-select outlined required :items="institutions" v-model="form.inst_id" value="current_user.inst_id"
+                          label="Institution" item-text="name" item-value="id"
+                ></v-select>
+              </div>
+              <div v-else>
+                <v-text-field outlined readonly label="Institution" :value="inst_name"></v-text-field>
+              </div>
+              <v-text-field v-model="form.server_url_r5" label="SUSHI Service URL" outlined></v-text-field>
+              <div class="field-wrapper has-label">
+                <v-subheader v-text="'Run Harvests Monthly on Day'"></v-subheader>
+                <v-text-field v-model="form.day_of_month" label="Day-of-Month" hide-details single-line type="number"
+                ></v-text-field>
+              </div>
+              <div class="field-wrapper has-label">
+                <v-subheader v-text="'Reports to Harvest'"></v-subheader>
+                <v-select :items="master_reports" v-model="form.master_reports" value="provider.reports" item-value="id"
+                          item-text="name" label="Select" multiple chips hint="Choose which reports to harvest"
+                          persistent-hint
+                ></v-select>
+              </div>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-col class="d-flex">
+              <v-btn class='btn' x-small color="primary" type="submit">Save New Provider</v-btn>
+            </v-col>
+            <v-col class="d-flex">
+              <v-btn class='btn' x-small type="button" color="primary" @click="provDialog=false">Cancel</v-btn>
+            </v-col>
+          </v-card-actions>
+        </form>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -108,8 +138,9 @@
       return {
         success: '',
         failure: '',
+        provDialog: false,
+        importDialog: false,
         inst_name: '',
-        showForm: '',
         headers: [
           { text: 'Provider ', value: 'prov_name', align: 'start' },
           { text: 'Status', value: 'is_active' },
@@ -128,13 +159,15 @@
         }),
         dtKey: 1,
         mutable_options: {},
+        csv_upload: null,
       }
     },
     methods:{
         importForm () {
             this.csv_upload = null;
             this.import_type = '';
-            this.showForm = 'import';
+            this.importDialog = true;
+            this.provDialog = false;
         },
         createForm () {
             this.failure = '';
@@ -145,10 +178,8 @@
             this.form.server_url_r5 = '';
             this.form.day_of_month = 15;
             this.form.master_reports = [];
-            this.showForm = 'create';
-        },
-        hideForm (event) {
-            this.showForm = '';
+            this.provDialog = true;
+            this.importDialog = false;
         },
         importSubmit (event) {
             this.success = '';
@@ -175,7 +206,7 @@
                          this.failure = response.data.msg;
                      }
                  });
-            this.showForm = '';
+             this.importDialog = false;
         },
         formSubmit (event) {
             this.success = '';
@@ -197,7 +228,7 @@
                         this.failure = response.msg;
                     }
                 });
-            this.showForm = '';
+            this.provDialog = false;
         },
         updateOptions(options) {
             if (Object.keys(this.mutable_options).length === 0) return;

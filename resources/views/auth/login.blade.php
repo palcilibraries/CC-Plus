5f740d81454177427b9ae7c3c84748aacca3aadd
Scott Ross <lsr2912@gmail.com>
2021-02-26 12:11:37 -0500

    restore login page submit button (last commit should not have deleted it)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -101,20 +101,21 @@ if (sizeof($consortia) == 1 )
                                 </div>
                             </div>
                         </div>
+-->
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+<!--
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+-->
                             </div>
                         </div>
--->
                     </form>
                 </div>
             </div>

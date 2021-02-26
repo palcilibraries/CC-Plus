1b43fa6ae0dee80feae7a5da6cfc8258c852c25f
Scott Ross <lsr2912@gmail.com>
2021-02-26 12:08:13 -0500

    Remove "forgot password" link from login page until it actually works properly.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -101,7 +101,6 @@ if (sizeof($consortia) == 1 )
                                 </div>
                             </div>
                         </div>
--->
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -115,6 +114,7 @@ if (sizeof($consortia) == 1 )
                                 @endif
                             </div>
                         </div>
+-->
                     </form>
                 </div>
             </div>

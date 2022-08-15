cbb958a8e6b541f47b5da958cdf6d8d9406c09b4
Scott Ross <lsr2912@gmail.com>
2022-08-15 15:01:05 -0400

    Add favicon to images folder and application  layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="icon" href="{!! asset('images/favicon.ico') !!}"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">

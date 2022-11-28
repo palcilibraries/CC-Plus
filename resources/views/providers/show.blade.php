fb763f9704dae92dac36150c7d59ba6e3a3207c8
Scott Ross <lsr2912@gmail.com>
2022-11-28 11:45:31 -0500

    Insitution and Provider blade files for show() Merged separate components and their properties into one component

--- resources/views/providers/show.blade.php
+++ resources/views/providers/show.blade.php
@@ -4,33 +4,10 @@
 <v-app providerform>
     <provider-form :provider="{{ json_encode($provider) }}"
                    :institutions="{{ json_encode($institutions) }}"
+                   :unset="{{ json_encode($unset_institutions) }}"
                    :master_reports="{{ json_encode($master_reports) }}"
                    :all_fields="{{ json_encode($all_fields) }}"
+                   :harvests="{{ json_encode($harvests) }}"
     ></provider-form>
-	  <div class="related-list">
-      <v-expansion-panels><v-expansion-panel>
-  	    <v-expansion-panel-header>
-          <h3>Recent Harvest Activity</h3>
-  	    </v-expansion-panel-header>
-  	    <v-expansion-panel-content>
-  	      @if (sizeof($harvests) > 0)
-            <harvestlog-summary-table :harvests="{{ json_encode($harvests) }}"
-                                      :prov_id="{{ $provider->id }}"
-            ></harvestlog-summary-table>
-  	      @else
-            <p>No harvest records found for this provider</p>
-          @endif
-  	    </v-expansion-panel-content>
-	    </v-expansion-panel></v-expansion-panels>
-    </div>
-    <div class="related-list">
-      <h2 class="section-title">Institutions</h2>
-      @if (auth()->user()->hasRole("Admin"))<a href="/institutions/create" class="section-action">add new</a> @endif
-      <all-sushi-by-prov :settings="{{ json_encode($provider->sushiSettings) }}"
-                         :prov_id="{{ json_encode($provider->id) }}"
-                         :unset="{{ json_encode($unset_institutions) }}"
-                         :connectors="{{ json_encode($provider->connectors) }}"
-      ></all-sushi-by-prov>
-    </div>
 </v-app>
 @endsection

d122ef1a55d34e3cb37904f806ae4e645b90e489
Scott Ross <lsr2912@gmail.com>
2022-11-29 11:48:08 -0500

    Navbar component pull out shadow class from bounding div to remove separating border between it and page content

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,5 +1,5 @@
   <template>
-    <nav class="navbar navbar-expand navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand navbar-light bg-white">
       <div class="container">
 	      <a class="navbar-brand" href="/">
           <img src="/images/CC_Plus_Logo.png" alt="CC plus" height="50px" width="103px" />

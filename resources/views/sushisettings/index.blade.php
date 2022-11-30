d9d084ba714956abf4a2e7eeaa2f5157bd342de2
Scott Ross <lsr2912@gmail.com>
2022-11-30 13:09:51 -0500

    Sushisettings datatable blade update Page had no Header row. Added as h3

--- resources/views/sushisettings/index.blade.php
+++ resources/views/sushisettings/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row">
+  <div class="col-lg-12 margin-tb">
+    <div class="pull-left">
+      <h3>{{ session('ccp_con_key','') }} : Sushi Settings</h3>
+    </div>
+  </div>
+</div>
 <v-app>
   <sushisettings-data-table :all_connectors="{{ json_encode($all_connectors) }}"
                          :institutions="{{ json_encode($institutions) }}"

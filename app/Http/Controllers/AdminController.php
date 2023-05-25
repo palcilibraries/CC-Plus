09a0cfd377bcd78b078138e577425d9cda2f042e
Scott Ross <lsr2912@gmail.com>
2023-05-25 13:41:33 -0400

    AdminController - Updates to support consortium admin home page. Add group names to institutions, and connection fields and report-state to providers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -33,9 +33,22 @@ class AdminController extends Controller
         $roles = Role::orderBy('id', 'ASC')->get(['name', 'id']);
         $viewRoleId = $roles->where('name', 'Viewer')->first()->id;
 
-        // Get institutions and groups
-        $institutions = Institution::orderBy('name', 'ASC')->get(['id','name'])->toArray();
-        $groups = InstitutionGroup::orderBy('name', 'ASC')->get(['id','name'])->toArray();
+        // Get institutions
+        $institutionData = Institution::orderBy('name', 'ASC')->get(['id','name']);
+
+        // Get institution groups , include members and non-members
+        $data = InstitutionGroup::with('institutions:id,name')->orderBy('name', 'ASC')->get();
+        $groups = array();
+        foreach ($data as $group) {
+            $group->count = $group->institutions->count();
+            if ($group->count > 0) {
+                $members = $group->institutions->pluck('id')->toArray();
+                $group->not_members = $institutionData->except($members);
+            } else {
+                $group->not_members = [];
+            }
+            $groups[] = $group->toArray();
+        }
 
         // Get master report definitions
         $master_reports = Report::where('revision',5)->where('parent_id',0)->orderBy('name','ASC')->get(['id','name']);
@@ -50,6 +63,7 @@ class AdminController extends Controller
             $rec->active = ($rec->is_active) ? 'Active' : 'Inactive';
             $rec->inst_name = ($rec->institution->id == 1) ? 'Entire Consortium' : $rec->institution->name;
             $rec->day_of_month = $rec->day_of_month;
+            $rec->connectors = $rec->globalProv->connectionFields();
             $last_harvest = $rec->sushiSettings->max('last_harvest');
             $rec->can_delete = (is_null($last_harvest)) ? true : false;
             if ($rec->reports) {
@@ -63,12 +77,18 @@ class AdminController extends Controller
             } else {
                 $rec->reports_string = "None";
             }
+            $rpt_state = [];
+            foreach ($master_reports as $rpt) {
+                $rpt_state[$rpt->name] = ($rec->reports->where('name',$rpt->name)->first()) ? true : false;
+            }
+            $rec->report_state = $rpt_state;
             return $rec;
         })->toArray();
 
         // Get global provider definitions for unconnected providers
         $existingIds = $provider_data->pluck('global_id')->toArray();
         $unset_global = GlobalProvider::whereNotIn('id',$existingIds)->orderBy('name', 'ASC')->get();
+        $institutions = $institutionData->toArray();
 
         // Load the view with the data
         return view('admin.home', compact('conso_name','roles','institutions','groups','providers','master_reports',

16f51819ddbafcd46afaaa95fa085ef42c325f35
Scott Ross <lsr2912@gmail.com>
2023-06-15 10:55:44 -0400

    Harvestlog Summary component - minor layout change

--- resources/js/components/HarvestlogSummaryTable.vue
+++ resources/js/components/HarvestlogSummaryTable.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <v-data-table :headers="headers" :items="mutable_harvests" item-key="id" class="elevation-1"
-                  :hide-default-footer="true" :server-items-length="10" dense disable-sort>
-      <template v-slot:item="{ item }">
-        <tr>
-          <td>{{ item.updated_at.substr(0,10) }}</td>
-          <td>{{ item.sushi_setting.institution.name }}</td>
-          <td>{{ item.sushi_setting.provider.name }}</td>
-          <td>{{ item.report.name }}</td>
-          <td>{{ item.yearmon }}</td>
-          <td>{{ item.attempts }}</td>
-          <td>{{ item.status }}</td>
-          <td v-if="item.attempts>0">
-            <a :href="'/harvests/'+item.id+'/edit'">details</a>
-          </td>
-        </tr>
-      </template>
-    </v-data-table>
-	<p class="more">
-		<a :href="seemore_url">See all harvests</a>
-	</p>
+    <div v-if="harvests.length > 0">
+      <v-data-table :headers="headers" :items="mutable_harvests" item-key="id" class="elevation-1"
+                    :hide-default-footer="true" :server-items-length="10" dense disable-sort>
+        <template v-slot:item="{ item }">
+          <tr>
+            <td>{{ item.updated_at.substr(0,10) }}</td>
+            <td>{{ item.sushi_setting.institution.name }}</td>
+            <td>{{ item.sushi_setting.provider.name }}</td>
+            <td>{{ item.report.name }}</td>
+            <td>{{ item.yearmon }}</td>
+            <td>{{ item.attempts }}</td>
+            <td>{{ item.status }}</td>
+            <td v-if="item.attempts>0">
+              <a :href="'/harvests/'+item.id+'/edit'">details</a>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </div>
+    <div v-else>
+      <p>No harvest records found for this institution</p>
+    </div>
+    <p class="more">
+      <a :href="seemore_url">See all harvests</a>
+    </p>
   </div>
 </template>
 

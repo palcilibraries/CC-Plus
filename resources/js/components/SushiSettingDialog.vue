6e72b598f1e28ce058f8922c2890a28e6dd47966
Scott Ross <lsr2912@gmail.com>
2024-03-12 17:49:11 -0400

    inst and prov form values will be different when creating.vs.editting a setting

--- resources/js/components/SushiSettingDialog.vue
+++ resources/js/components/SushiSettingDialog.vue
@@ -105,13 +105,14 @@
     watch: {
       sushi_inst: function (inst) {
         this.form.inst_id = this.sushi_inst.id;
+        this.form.inst_id = (this.mutable_dtype == 'edit') ? this.setting.inst_id : this.sushi_inst.id;
         if (this.all_settings.length>0 && this.form.prov_id != null && this.form.inst_id != null) {
           this.testExisting();
           this.form_key += 1;
         }
       },
       sushi_prov: function (prov) {
-        this.form.prov_id = this.sushi_prov.conso_id;
+        this.form.prov_id = (this.mutable_dtype == 'edit') ? this.setting.prov_id : this.sushi_prov.conso_id;
         if (this.all_settings.length>0 && this.form.prov_id != null && this.form.inst_id != null) {
           this.testExisting();
           this.form_key += 1;
@@ -136,7 +137,7 @@
           this.failure = '';
           this.form.inst_id = this.sushi_inst.id;
           this.form.global_id = this.sushi_prov.id;
-          this.form.prov_id = this.sushi_prov.conso_id;
+          this.form.prov_id = (this.mutable_dtype == 'edit') ? this.setting.prov_id : this.sushi_prov.conso_id;
           this.form.status = this.statusval;
           // All connectors are required - whether they work or not is a matter of testing+confirming
           this.sushi_prov.connectors.forEach( (cnx) => {

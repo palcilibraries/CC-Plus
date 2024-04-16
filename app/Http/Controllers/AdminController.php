d046e42fe0193cb6c8fc34a297f02cfdb1900b38
Scott Ross <lsr2912@gmail.com>
2024-04-16 16:55:33 -0400

    Change handling of "connected" institution arrays and connection-count for providers, based on whether the provider is inst-specific or consortium-wide

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -79,50 +79,64 @@ class AdminController extends Controller
             $rec->active = ($rec->is_active) ? 'Active' : 'Inactive';
             $rec->day_of_month = null;
             $rec->can_delete = false;
-            $rec->connected = array();
             $reports_string = ($rec->master_reports) ?
-                               $this->makeReportString($rec->master_reports, $master_reports) : '';
-            $report_state = $this->reportState($rec->master_reports, $master_reports);
-            $connected_providers = $conso_providers->where('global_id',$rec->id);
-            $connected = (count($connected_providers) > 0);
-            $conso_connection = $connected_providers->where('inst_id',1)->first();
-            // Include unconnected globals in the array
-            if (!$connected) {
-                $rec->report_state = $report_state;
-                $rec->reports_string = ($reports_string == '') ? "None" : $reports_string;
-                $output_providers[] = $rec->toArray();
-            // Include all providers connected to the global in the array
-            } else {
-                foreach ($connected_providers as $prov_data) {
-                    if ($prov_data->inst_id == 1) {
-                        $_insts = $connected_providers->pluck('institution')->sortBy('name');
-                        $rec->connected = $_insts->map( function ($_inst) {
-                          if ($_inst->id == 1) $_inst->name = 'Entire Consortium';
-                          return $_inst;
-                        })->values()->toArray();
-                        $rec->connection_count = count($rec->connected);
-                    } else {
-                        $rec->connected = array($prov_data->institution->name);
-                        $rec->connection_count = 1;
-                    }
-                    $rec->inst_id = $prov_data->inst_id;
-                    $rec->can_edit = true;
-                    $rec->can_connect = ($conso_connection && $rec->inst_id==1) ? false : true;
-                    $rec->conso_id = $prov_data->id;
-                    $rec->is_active = $prov_data->is_active;
-                    $rec->active = ($prov_data->is_active) ? 'Active' : 'Inactive';
-                    $rec->day_of_month = $prov_data->day_of_month;
-                    $rec->last_harvest = $prov_data->sushiSettings->max('last_harvest');
-                    $rec->restricted = $prov_data->restricted;
-                    $rec->allow_inst_specific = $prov_data->allow_inst_specific;
-                    $rec->can_delete = (is_null($rec->last_harvest)) ? true : false;
-                    if ($prov_data->reports) {
-                        $report_ids = $prov_data->reports->pluck('id')->toArray();
-                        $reports_string = $this->makeReportString($report_ids, $master_reports);
-                        $report_state = $this->reportState($report_ids, $master_reports);
-                    }
-                    $output_providers[] = $rec->toArray();
+                                   $this->makeReportString($rec->master_reports, $master_reports) : '';
+            $rec->report_state = $this->reportState($rec->master_reports, $master_reports);
+
+            // Remap master reports to just the globally available ones and add names
+            $_reports = [];
+            foreach ($master_reports as $rpt) {
+                if (in_array($rpt->id, $rec->master_reports)) {
+                    $_reports[] = array('id' => $rpt->id, 'name' => $rpt->name);
                 }
+            }
+            $rec->master_reports = $_reports;
+            $rec->reports_string = ($reports_string == '') ? "None" : $reports_string;
+
+            // Setup connected institution data for all outpute records
+            $connected_insts = array();
+            $connected_providers = $conso_providers->where('global_id',$rec->id);
+            foreach ($connected_providers as $prov_data) {
+                $_name = ($prov_data->inst_id == 1) ? 'Entire Consortium' : $prov_data->institution->name;
+                $connected_insts[] = array('id' => $prov_data->instid, 'name' => $_name);
+            }
+
+            // Include globals not connected to the consortium in the array
+            $conso_connection = $connected_providers->where('inst_id',1)->first();
+            if (!$conso_connection) {
+                $rec->connected = array();
+                $rec->connection_count = 0;
+                $output_providers[] = $rec->toArray();
+            }
+
+            // Include all connected providers connected to the global in the array
+            foreach ($connected_providers as $prov_data) {
+                $rec->inst_id = $prov_data->inst_id;
+                $rec->inst_name = $prov_data->institution->name;
+                // inst-specific providers show only one connection; consortium providers include all
+                $rec->connected = ($rec->inst_id==1) ? $connected_insts
+                                                     : array( array('id' => $rec->inst_id, 'name' => $rec->inst_name) );
+                $rec->connection_count = count($rec->connected);
+                $rec->can_edit = true;
+                $rec->conso_id = $prov_data->id;
+                $rec->is_active = $prov_data->is_active;
+                $rec->active = ($prov_data->is_active) ? 'Active' : 'Inactive';
+                $rec->day_of_month = $prov_data->day_of_month;
+                $rec->last_harvest = $prov_data->sushiSettings->max('last_harvest');
+                $rec->restricted = $prov_data->restricted;
+                $rec->allow_inst_specific = $prov_data->allow_inst_specific;
+                if ($conso_connection) {
+                    $rec->can_connect = ($conso_connection->allow_inst_specific && $rec->inst_id == 1) ? true : false;
+                } else {
+                    $rec->can_connect = ($rec->inst_id == 1) ? true : false;
+                }
+                $rec->can_delete = (is_null($rec->last_harvest)) ? true : false;
+                if ($prov_data->reports) {
+                    $report_ids = $prov_data->reports->pluck('id')->toArray();
+                    $rec->reports_string = $this->makeReportString($report_ids, $master_reports);
+                    $rec->report_state = $this->reportState($report_ids, $master_reports);
+                }
+                $output_providers[] = $rec->toArray();
             }
         }
         $providers = array_values($output_providers);

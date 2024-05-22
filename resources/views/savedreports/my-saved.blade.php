ae18cd8449a0f227c72baf39dcd49a7f26e35c10
Scott Ross <lsr2912@gmail.com>
2024-05-22 12:30:36 -0400

    Fix page-title for saved reports page

--- resources/views/savedreports/my-saved.blade.php
+++ resources/views/savedreports/my-saved.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('page_title')
+    {{ "My Reports -- CC-Plus" }}
+@endsection
 @section('content')
 <home-saved-reports :reports="{{ json_encode($report_data) }}"
                     :counter_reports="{{ json_encode($counter_reports) }}"

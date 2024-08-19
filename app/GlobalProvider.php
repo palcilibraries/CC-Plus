351724c654fbef3db92edea28fda6a8ba55909ed
Scott Ross <lsr2912@gmail.com>
2024-08-19 15:41:47 -0400

    Update class to support globalProviders being the relationship for sushi and harvested data provider-Ids. Includes new methods to support controller and U/I functionality

--- app/GlobalProvider.php
+++ app/GlobalProvider.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace App;
+use App\Report;
 use App\ConnectionField;
 use Illuminate\Database\Eloquent\Model;
 
 class GlobalProvider extends Model
 {
+  /**
+   * Class Constructor
+   */
+    private $global_masters;
+    public function __construct(array $attributes = array())
+    {
+        parent::__construct($attributes);
+        $this->global_masters = Report::where('parent_id',0)->get();
+    }
   /**
    * The database table used by the model.
    *
@@ -31,7 +41,84 @@ class GlobalProvider extends Model
     public function connectionFields()
     {
         $cnxs = $this->connectors;
-        if (count($cnxs) == 0) return [];
-        return ConnectionField::whereIn('id',$cnxs)->get()->toArray();
+        return (count($cnxs) == 0) ? [] : ConnectionField::whereIn('id',$cnxs)->get()->toArray();
+    }
+
+    public function sushiSettings()
+    {
+        return $this->hasMany('App\SushiSetting', 'prov_id');
+    }
+
+    public function titleReports()
+    {
+        return $this->hasMany('App\TitleReport');
+    }
+
+    public function databaseReports()
+    {
+        return $this->hasMany('App\DatabaseReport');
+    }
+
+    public function platformReports()
+    {
+        return $this->hasMany('App\PlatformReport');
+    }
+
+    public function itemReports()
+    {
+        return $this->hasMany('App\ItemReport');
+    }
+
+    public function consoProviders()
+    {
+        return $this->hasMany('App\Provider', 'global_id');
+    }
+
+
+    public function isComplete()
+    {
+        $required = $this->provider->connectors;
+        $connectors = $this->global_connectors->whereIn('id',$required)->pluck('name')->toArray();
+        foreach ($connectors as $cnx) {
+            if (is_null($this->$cnx) || trim($this->$cnx) == '' || $this->$cnx == '-required-') return false;
+        }
+        return true;
+    }
+
+    // Return an array of connected institution IDs
+    // NOTE: caller should include with("consoProviders") before using this on a collection
+    public function connectedInstitutions()
+    {
+        if ($this->consoProviders->count() == 0) return [];
+        return $this->consoProviders->pluck('inst_id')->toArray();
+    }
+
+    // Build and return an array of by-report assignments
+    // NOTE: caller should include with("consoProviders","consoProviders.reports") before using this on a collection
+    public function enabledReports()
+    {
+        // Setup array for each master report
+        foreach ($this->global_masters as $mr) {
+            $reports[$mr->name] = array();
+        }
+        if ($this->consoProviders->count() == 0) return $reports;
+
+        // get consortium-wide settings
+        $consoWide = $this->consoProviders->where('inst_id',1)->first();
+        if ($consoWide) {
+            foreach ($consoWide->reports as $rpt) {
+                $reports[$rpt->name] = "ALL";
+            }
+        }
+
+        // Get/build inst-specific assignments
+        $instSpecific = $this->consoProviders->where('inst_id','<>',1);
+        foreach ($instSpecific as $instProv) {
+            foreach ($instProv->reports as $rpt) {
+                if ($reports[$rpt->name] == "ALL") continue;
+                $reports[$rpt->name][] = $instProv->inst_id;
+            }
+        }
+        return $reports;
     }
 }

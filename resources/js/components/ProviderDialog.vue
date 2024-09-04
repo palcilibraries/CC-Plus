01d448068d5020a5f35d5a77ae9cbe7951201e46
Scott Ross <lsr2912@gmail.com>
2024-09-04 17:53:20 -0400

    allow local admins to toggle status on inst-specific providers, remove day-of-month handling, update() events now pass the global_id instead of the conso-provider-id

--- resources/js/components/ProviderDialog.vue
+++ resources/js/components/ProviderDialog.vue
@@ -55,7 +55,7 @@
         </v-row>
       </div>
       <v-row class="d-flex mx-2 my-0" no-gutters>
-        <v-col class="d-flex px-2" cols="3">
+        <v-col v-if="is_admin || (is_manager && form.inst_id!=1)" class="d-flex px-2" cols="3">
           <v-switch v-model="form.is_active" dense label="Active?" :disabled="mutable_dtype=='edit' && !is_admin &&
                                                                               provider.inst_id==1"></v-switch>
         </v-col>
@@ -70,7 +70,6 @@
         <v-col v-else class="d-flex px-4 justify-center" cols="6">
           <strong>No reports enabled globally</strong>
         </v-col>
-        <v-col class="d-flex px-6" cols="6"><strong>Run Harvests Monthly on Day</strong></v-col>
       </v-row>
       <v-row class="d-flex mx-2 my-0" no-gutters>
         <v-col v-if="mutable_provider.master_reports.length>0" class="d-flex px-4 justify-center" cols="6">
@@ -85,11 +84,6 @@
           <div class="float-none"></div>
         </v-col>
         <v-col v-else class="d-flex" cols="6">&nbsp;</v-col>
-        <v-col class="d-flex pl-8" cols="2">
-          <v-text-field v-model="form.day_of_month" label="Day-of-Month" single-line dense type="number"
-                        class="centered-input" :disabled="mutable_dtype=='edit' && !is_admin && provider.is_conso" :rules="dayRules"
-          ></v-text-field><br />
-        </v-col>
       </v-row>
       <v-row v-if="mutable_provider.last_harvest!=null" class="d-flex mx-2 my-0" no-gutters>
         <v-col class="d-flex px-6" cols="6">&nbsp;</v-col>
@@ -125,11 +119,6 @@
       return {
         mutable_provider: {},
         formValid: true,
-        dayRules: [
-            v => !!v || "Day of month is required",
-            v => ( v && v >= 1 ) || "Day of month must be > 1",
-            v => ( v && v <= 28 ) || "Day of month must be < 29",
-        ],
         warn_inst: '',
         current_inst_name: '',
         mutable_dtype: this.dtype,
@@ -137,7 +126,6 @@
             name: '',
             inst_id: 1,
             global_id: null,
-            day_of_month: 15,
             is_active: 1,
             allow_inst_specific: 0,
             sushi_stub: 1,
@@ -149,16 +137,16 @@
     methods: {
       saveProv (event) {
           if (this.mutable_dtype == 'edit') {
-            this.form.patch('/providers/'+this.mutable_provider['id'])
+            this.form.patch('/providers/'+this.mutable_provider['global_id'])
                 .then( (response) => {
-                    var _prov   = (response.result) ? response.provider : null;
+                    var _prov   = (response.result) ? Object.assign({},response.provider) : null;
                     var _result = (response.result) ? 'Success' : 'Fail';
                     this.$emit('prov-complete', { result:_result, msg:response.msg, prov:_prov });
                 }).catch({});
           } else {
             this.form.post('/providers/connect')
                 .then( (response) => {
-                    var _prov   = (response.result) ? response.provider : null;
+                    var _prov   = (response.result) ? Object.assign({},response.provider) : null;
                     var _result = (response.result) ? 'Success' : 'Fail';
                     this.$emit('prov-complete', { result:_result, msg:response.msg, prov:_prov });
                 }).catch({});
@@ -185,7 +173,7 @@
         // Chosen inst is not yet connected
         } else {
           let _inst = this.institutions.find(ii => ii.id == this.form.inst_id);
-          this.current_inst_name = _prov.inst_name;
+          this.current_inst_name = (typeof(_inst) == 'undefined') ? "" : _inst.name;
 
           // Flip dtype to connect
           this.mutable_dtype = "connect";
@@ -218,7 +206,6 @@
         // Setup initial form fields based on provider
         this.form.name = this.mutable_provider.name;
         this.form.global_id = this.mutable_provider.global_id;
-        this.form.day_of_month = this.mutable_provider.day_of_month;
         this.form.is_active = this.mutable_provider.is_active;
         this.form.allow_inst_specific = this.mutable_provider.allow_inst_specific;
         this.form.report_state = Object.assign({},this.mutable_provider.report_state);
